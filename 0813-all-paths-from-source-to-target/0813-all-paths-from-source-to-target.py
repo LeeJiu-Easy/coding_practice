27624c18424b069b0ee161d4145ce0a3ebd9599d
이지우 <131653682+LeeJiu-Easy@users.noreply.github.com>
2023-08-09 09:49:37 +0900

    Time: 85 ms (91.85%), Space: 18.5 MB (36.84%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -1,17 +1,13 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         from collections import deque
-
-        q = deque([(graph[0], [0])])
+        q = deque([(0, [])])
         answer = []
-        
         while q:
-            cur, path = q.popleft()
+            cur_node, path = q.popleft()
+            if cur_node == len(graph)-1:
+                answer.append(path + [cur_node])
 
-            for pos in cur:
-                new_path = path + [pos]
-                q.append((graph[pos], new_path))
-                if pos == len(graph)-1:
-                    answer.append(new_path)
-
+            for node in graph[cur_node]:
+                q.append((node, path + [cur_node]))
         return answer
